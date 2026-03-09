863b3ccc72a357f3fe5ca7d467d34de5af8cfca8
masudabdullaa <masudabdullaa@gmail.com>
2026-03-09 07:40:39 +0600

    button selection part done next part is button functionality

--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
+const allIssues = document.getElementById("issue-card-container");
+
+
 function viewIssues() {
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
     fetch(url)
@@ -21,20 +24,20 @@ function viewIssues() {
 //     "updatedAt": "2024-01-25T11:00:00Z"
 // }
 
-const allIssues = document.getElementById("issue-card-container");
 allIssues.innerHTML = "";
 
 const displayIssue = (issues) => {
     // console.log(issues);
     issues.forEach(issue => {
-        console.log(issue);
+        // console.log(issue);
         const cards = document.createElement("div");
         cards.innerHTML = `
-    <div class="rounded-lg p-3 shadow-lg space-y-5">
-                <div class="flex justify-between">
-                <div>${issue.status == "open"? `<img class="w-[40px]" src="./assets/Open-Status.png" alt="">`: `<img src="./assets/Closed- Status .png" alt="">`}</div>
+    <div class="rounded-lg p-3 shadow-lg space-y-5 h-full">
+                <div class="flex justify-between items-center">
+                <div>${issue.status == "open" ? `<img class="w-[40px]" src="./assets/Open-Status.png" alt="">`
+                : `<img class="w-[40px]" src="./assets/Closed- Status .png" alt="">`}</div>
                     
-                    <button class="badge rounded-full py-4 px-5 bg-rose-200 font-bold text-rose-800">${issue.priority}</button>
+                    <button class="rounded-full py-4 px-5 font-bold text-lg">${issue.priority}</button>
                 </div>
                 <h2 class="font-bold text-xl">${issue.title}</h2>
                 <p class="text-gray-500">${issue.description}</p>
@@ -42,13 +45,34 @@ const displayIssue = (issues) => {
                     <button class="btn btn-outline btn-secondary rounded-full">BUG</button>
                     <button class="btn btn-outline btn-warning rounded-full">HELP WANTED</button>
                 </div>
-                <p>1 by john_doe</p>
-                <p>1/15/2024</p>
+                <p>${issue.author}</p>
+                <p>${issue.createdAt}</p>
             </div>
     `;
+
         allIssues.appendChild(cards);
     });
 
 }
 
 viewIssues();
+
+let currentbtn = "all";
+const btnActive = ["btn-primary"];
+const btnInactive = ["bg-transparent", "text-slate-700", "border-slate-200"];
+
+function loadIssues(btn){
+    // console.log(btn);
+    const btns = ["all", "open", "closed"];
+    for(const button of btns){
+        const btnName = document.getElementById("btn-" + button);
+        if(button == btn){
+            btnName.classList.remove(...btnInactive);
+            btnName.classList.add(...btnActive);
+        }else{
+            btnName.classList.add(...btnInactive);
+            btnName.classList.remove(...btnActive);
+        } 
+    }
+}
+loadIssues(currentbtn);
